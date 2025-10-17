29fa8a91c8b29ecf5162dcfa32d0ccd89f451161
Humbalicious-neitzen <humbal.awan@tkxel.io>
2025-10-17 17:51:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ensure relative paths work on Render
-  root: '.',
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: './index.html',
-      },
+      input: './index.html',
     },
   },
 });
